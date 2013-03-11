fb24f377527ff1da9b21ba7142dd47c774543d9b
Andrew Panfilov <andrew@dzer6.com>
2013-03-11 23:00:42 +0200

    refactor session storage implementation

--- vc/ga3/src/main/groovy/com/dzer6/ga3/services/SessionStorageService.groovy
+++ vc/ga3/src/main/groovy/com/dzer6/ga3/services/SessionStorageService.groovy
@@ -1,7 +1,7 @@
 package com.dzer6.ga3.services
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import com.dzer6.vc.session.storage.SessionNotFoundException
 import com.dzer6.vc.session.storage.SessionStorage
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Service
 
 @Service("sessionStorageService")
 @Scope("singleton")
-@Transactional
 class SessionStorageService implements SessionStorage {
 
     private static final Logger log = LoggerFactory.getLogger(SessionStorageService.class)
@@ -39,63 +38,67 @@ class SessionStorageService implements SessionStorage {
     @Delegate
     private ServicesUtil servicesUtil = new ServicesUtil()
 
+    @Transactional
     @Override
     String createSession() {
         Session session = new Session()
         long sessionLiveTime = config.SESSION_LIVE_TIME as long
         session.lastUserDisconnection = new Date(System.currentTimeMillis() + sessionLiveTime)
         sessionRepository.save(session)
+        sessionRepository.flush()
     
         log.info("createSession(), session.id = ${session.id}")
         return session.id
+    }
+    
+    @Transactional(readOnly = true)
+    Session getSession(String sessionId) {
+        assert sessionId != null 
+        assert sessionId != ""
+        Session session = sessionRepository.findOne(sessionId)
+        if (session == null) {
+            throw new SessionNotFoundException(sessionId)
+        }
+        return session
     }
 
     @Override
     void disposeSession(String sessionId) {
         log.info("disposeSession(${sessionId})")
+        assert sessionId != null 
+        assert sessionId != ""
         invokeAgainIfOptimisticLockingFailureCatched("SessionStorageService.disposeSession(${sessionId})") {
-            Session session = sessionRepository.findOne(sessionId)
-            if (session == null) {
-                throw new SessionNotFoundException(sessionId)
-            }
+            Session session = getSession(sessionId)
             sessionDataRepository.deleteAllBySession(session)
             sessionRepository.delete(session)
         }
     }
   
+    @Transactional(readOnly = true)
     boolean sessionExists(String sessionId) {
         log.info("sessionExists(${sessionId})")
         return sessionId != null && sessionId != "" && sessionRepository.findOne(sessionId) != null
     }
 
+    @Transactional(readOnly = true)
     @Override
     Serializable get(String sessionId, String key) {
         log.info("get(${sessionId}, ${key})")
-        if (sessionId == null || sessionId == "") {
-            throw new SessionNotFoundException(sessionId)
-        }
+        assert sessionId != null 
+        assert sessionId != ""
     
-        Session session = sessionRepository.findOne(sessionId)
-        if (session == null) {
-            throw new SessionNotFoundException(sessionId)
-        }
-
+        Session session = getSession(sessionId)
         return sessionDataRepository.findBySessionAndKey(session, key)[0]?.data
     }
 
     @Override
     void put(String sessionId, String key, Serializable value) {
         log.info("put(${sessionId}, ${key})")
-        if (sessionId == null || sessionId == "") {
-            throw new SessionNotFoundException(sessionId)
-        }
+        assert sessionId != null 
+        assert sessionId != ""
     
-        Session session = sessionRepository.findOne(sessionId)
-        if (session == null) {
-            throw new SessionNotFoundException(sessionId)
-        }
-
         invokeAgainIfOptimisticLockingFailureCatched("SessionStorageService.put(${sessionId}, ${key})") {
+            Session session = getSession(sessionId)
             SessionData sessionData = sessionDataRepository.findBySessionAndKey(session, key)[0]
             if (sessionData == null) {
                 sessionData = new SessionData()
@@ -110,13 +113,14 @@ class SessionStorageService implements SessionStorage {
     @Override
     void remove(String sessionId, String key) {
         log.info("remove(${sessionId}, ${key})")
+        assert sessionId != null 
+        assert sessionId != ""
+        
+        assert key != null 
+        assert key != ""
     
-        Session session = sessionRepository.findOne(sessionId)
-        if (session == null) {
-            throw new SessionNotFoundException(sessionId)
-        }
-
         invokeAgainIfOptimisticLockingFailureCatched("SessionStorageService.remove(${sessionId}, ${key})") {
+            Session session = getSession(sessionId)
             sessionDataRepository.deleteBySessionAndKey(session, key)
         }
     }
@@ -125,12 +129,11 @@ class SessionStorageService implements SessionStorage {
     void connectionLost(String sessionId) {
         log.info("connectionLost(${sessionId})")
     
-        Session session = sessionRepository.findOne(sessionId)
-        if (session == null) {
-            throw new SessionNotFoundException(sessionId)
-        }
-
+        assert sessionId != null 
+        assert sessionId != ""
+        
         invokeAgainIfOptimisticLockingFailureCatched("SessionStorageService.connectionLost(${sessionId})") {
+            Session session = getSession(sessionId)
             session.lastUserDisconnection = new Date()
             sessionRepository.save(session)
         }
@@ -140,14 +143,12 @@ class SessionStorageService implements SessionStorage {
     void connectionStarted(String sessionId) {
         log.info("connectionStarted(${sessionId})")
     
-        Session session = sessionRepository.findOne(sessionId)
-        if (session == null) {
-            throw new SessionNotFoundException(sessionId)
-        }
-
-        long sessionLiveTime = config.SESSION_LIVE_TIME as long
-
+        assert sessionId != null 
+        assert sessionId != ""
+        
         invokeAgainIfOptimisticLockingFailureCatched("SessionStorageService.connectionStarted(${sessionId})") {
+            Session session = getSession(sessionId)
+            long sessionLiveTime = config.SESSION_LIVE_TIME as long
             session.lastUserDisconnection = new Date(System.currentTimeMillis() + sessionLiveTime)
             sessionRepository.save(session)
         }
